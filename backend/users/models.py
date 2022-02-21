0486cf7e9dd7a44a9aa6b5e901e5c7f01e6320ec
nezl0i <nezl0in@gmail.com>
2022-02-21 13:43:44 +0300

    init

---
+++ backend/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class RestUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    age = models.PositiveSmallIntegerField(default=18)
+
+    def __str__(self):
+        return self.username
+
+
---
+++ backend/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import RestUser
+from .serializers import RestUserSerializer
+
+
+class RestUserViewSet(ModelViewSet):
+    queryset = RestUser.objects.all()
+    serializer_class = RestUserSerializer
+    
