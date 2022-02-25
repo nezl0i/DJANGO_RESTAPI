497a6cd0a26857e51b3f2317a03bb5c278fe6b88
nezl0i <nezl0in@gmail.com>
2022-02-25 12:12:46 +0300

    create todoapp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'users',
+    'todoapp',
 ]
 
 MIDDLEWARE = [
