e6988dc3c9a50164cedaee869f12eadba9372dc7
nezl0i <nezl0in@gmail.com>
2022-02-25 20:09:36 +0300

    change views.py

--- backend/todoapp/views.py
+++ backend/todoapp/views.py
@@ -1,22 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ProjectSerializer, ToDoSerializer
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
-from rest_framework import status
-from django_filters.rest_framework import DjangoFilterBackend
 from .models import Project, ToDo
 
 
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
-
-    # def get_queryset(self):
-    #     queryset = Project.objects.all()
-    #     name = self.request.query_params.get('name', None)
-    #     if name:
-    #         queryset = queryset.filter(name__contains=name)
-    #     return queryset
 
 
 class ToDoViewSet(ModelViewSet):
