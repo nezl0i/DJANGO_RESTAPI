a859318b4bd4847c3a9e0d9ae63900b8e0e1ab1c
nezl0i <nezl0in@gmail.com>
2022-03-04 11:40:21 +0300

    add JWT token auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from todoapp.views import ToDoViewSet, ProjectViewSet
 from users.views import RestUserViewSet
 
@@ -15,4 +16,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('api-auth-token/', obtain_auth_token),
+    path('api-jwt-auth-token/', obtain_jwt_token),
+    path('api-jwt-token-refresh/', refresh_jwt_token),
+    path('api-jwt-token-verify/', verify_jwt_token),
 ]
