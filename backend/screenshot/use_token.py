0fa1420d542b6d1d9e3f754c081d0db2f50b18ae
nezl0i <nezl0in@gmail.com>
2022-03-25 11:33:58 +0300

    remove screenshots

--- backend/screenshot/use_token.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:8009/api-auth-token/'
-json = {'username': 'bob', 'password': 'restapi'}
-
-response = requests.post(url, json=json)
-
-print(response.status_code)
-print(response.json())
