57876556112e1d7d869e5b2e32c62889a4ab4a79
nezl0i <nezl0in@gmail.com>
2022-03-30 09:14:47 +0300

    add re-doc and swagger OpenAPI

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
 from todoapp.views import ToDoViewSet, ProjectViewSet
 from users.views import RestUserViewSet
 
@@ -10,9 +14,27 @@ router.register('users', RestUserViewSet)
 router.register('todo', ToDoViewSet)
 router.register('projects', ProjectViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Toto application backend",
+        default_version='0.1',
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('api-auth-token/', obtain_auth_token),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
